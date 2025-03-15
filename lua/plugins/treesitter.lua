77c45b79b38abdd35edcab0e4eef98eb147f1809
Manuel Stieger <manue.stieger@gmail.com>
2025-03-15 10:57:40 +0100

    removed unnecessary specs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -49,37 +49,9 @@ return {
 		{ "<c-space>", desc = "Increment Selection" },
 		{ "<bs>", desc = "Decrement Selection", mode = "x" },
 	},
-	opts_extend = { "ensure_installed" },
 	---@type TSConfig
 	---@diagnostic disable-next-line: missing-fields
 	opts = {
-		highlight = { enable = true },
-		indent = { enable = true },
 		ensure_installed = installedLanguages,
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<C-space>",
-				node_incremental = "<C-space>",
-				scope_incremental = false,
-				node_decremental = "<bs>",
-			},
-		},
-		textobjects = {
-			move = {
-				enable = true,
-				goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-				goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-				goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-				goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
-			},
-		},
 	},
-	---@param opts TSConfig
-	config = function(_, opts)
-		if type(opts.ensure_installed) == "table" then
-			opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
-		end
-		require("nvim-treesitter.configs").setup(opts)
-	end,
 }
