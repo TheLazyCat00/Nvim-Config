1c8dbd1858d13e842069dee8c9412d251fe41d02
Manuel Stieger <manue.stieger@gmail.com>
2025-03-21 09:46:08 +0100

    better keys

--- lua/plugins/racer-nvim.lua
+++ lua/plugins/racer-nvim.lua
@@ -1,6 +1,11 @@
 return {
 	"TheLazyCat00/racer-nvim",
-	opts = {},
+	opts = {
+		triggers = {
+			{"[", "]"},
+			{"F", "f"},
+		}
+	},
 	keys = {
 		{";", "<cmd>lua require('racer-nvim').prev()<CR>", mode = {"n", "x"}, desc = "Repeat previous"},
 		{",", "<cmd>lua require('racer-nvim').next()<CR>", mode = {"n", "x"}, desc = "Repeat next"},
