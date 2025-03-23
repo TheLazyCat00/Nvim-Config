299674a2233cf6c59a99b197dcf05cf858341d13
Manuel Stieger <manue.stieger@gmail.com>
2025-03-23 15:01:41 +0100

    update

--- lua/plugins/simple-format.lua
+++ lua/plugins/simple-format.lua
@@ -1,6 +1,7 @@
 return {
 	"TheLazyCat00/simple-format",
 	event = "BufReadPost",
+	opts = {},
 	config = function ()
 		vim.api.nvim_create_autocmd("InsertLeave", {
 			callback = function()
@@ -10,6 +11,7 @@ return {
 					replace("(<operator>)(%S)", "%1 %2")
 					replace("(%S)(<constructor>)", "%1 %2")
 					replace("(<constructor>)(%S)", "%1 %2")
+					replace("(<constructor>) (<constructor>)", "%1%2")
 					replace("(<constructor>) (<punctuation.bracket>)", "%1%2")
 					replace("(<punctuation.bracket>) (<constructor>)", "%1%2")
 					replace("(<constructor>) (<punctuation.delimiter>)", "%1%2")
