417e02680e6bcaeeb2a6a0c34f16963595e1aefc
Manuel Stieger <manue.stieger@gmail.com>
2025-04-24 16:43:52 +0200

    update replace-nvim

--- lua/plugins/replace-nvim.lua
+++ lua/plugins/replace-nvim.lua
@@ -2,8 +2,12 @@ return {
 	"TheLazyCat00/replace-nvim",
 	opts = {},
 	keys = {
-		{ "t", function()
-			return require('replace-nvim').replace()
-		end, expr = true, desc = "Replace with clipboard" }
-	}
+		{
+			"t",
+			function() return require('replace-nvim').replace(true) end,
+			mode = { "n", "x" },
+			expr = true,
+			desc = "Replace with clipboard",
+		},
+	},
 }
