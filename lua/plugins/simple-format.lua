218471191775e92307e6e4e100acffdb0f45cf48
Manuel Stieger <manue.stieger@gmail.com>
2025-11-30 00:11:07 +0100

    update

--- lua/plugins/simple-format.lua
+++ lua/plugins/simple-format.lua
@@ -1,22 +1,22 @@
 return {
 	"TheLazyCat00/simple-format",
 	event = "BufReadPost",
-	enabled = false,
 	opts = {},
 	config = function (_, opts)
-		local simple_format = require("simple-format")
-		simple_format.setup(opts)
-		local replace = simple_format.replace
+		local simpleFormat = require("simple-format")
+		simpleFormat.setup(opts)
+		local replace = simpleFormat.replace
 		vim.api.nvim_create_autocmd("InsertLeave", {
 			callback = function()
 				vim.schedule(function ()
-					replace("(%S) -(<operator=.->)", "%1 %2", true)
-					replace("(<operator=.->) -(%S)", "%1 %2", true)
-					replace("(<punctuation.delimiter=,>) -(%S)", "%1 %2")
-					replace("(<punctuation.bracket={>) -(%S.-%S) -(<punctuation.bracket=}>)", "%1 %2 %3")
-					replace("(<punctuation.bracket={>) -(<punctuation.bracket=}>)", "%1%2")
-					replace("(<punctuation.bracket=%(>) -(%S.-%S) -(<punctuation.bracket=%)>)", "%1%2%3")
-					replace("(<punctuation.bracket=%(>) -(<punctuation.bracket=%)>)", "%1%2")
+					-- simpleFormat.reveal() commented out right now, but this shows the line "structure"
+					replace("(%S) -(<.-|operator|.-=.->)", "%1 %2")
+					replace("(<.-|operator|.-=.->) -(%S)", "%1 %2")
+					replace("(<.-|punctuation.delimiter|.-=,>) -(%S)", "%1 %2")
+					replace("(<.-|punctuation.bracket|.-={>) -(<.-|punctuation.bracket|.-=}>)", "%1%2")
+					replace("(<.-|punctuation.bracket|.-={>) -(%S.-%S) -(<.-|punctuation.bracket|.-=}>)", "%1 %2 %3")
+					replace("(<.-|punctuation.bracket|.-=%(>) -(%S.-%S) -(<.-|punctuation.bracket|.-=%)>)", "%1%2%3")
+					replace("(<.-|punctuation.bracket|.-=%(>) -(<.-|punctuation.bracket|.-=%)>)", "%1%2")
 				end)
 			end,
 		})
