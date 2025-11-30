db0557953c2338f56d15bebc12dd9e9316e4c6b9
Manuel Stieger <manue.stieger@gmail.com>
2025-11-30 13:10:35 +0100

    update simple format

--- lua/plugins/simple-format.lua
+++ lua/plugins/simple-format.lua
@@ -6,19 +6,26 @@ return {
 		local simpleFormat = require("simple-format")
 		simpleFormat.setup(opts)
 		local replace = simpleFormat.replace
+
+		local function format()
+			vim.schedule(function ()
+				replace("(<.->) *(<|operator|=.->)","%1 %2")
+				replace("(<|operator|=.->) *(<.->)","%1 %2")
+
+				replace("(<.->) *(<|punctuation.delimiter|=,>)", "%1%2")
+				replace("(<|punctuation.delimiter|=,>) *(<.->)", "%1 %2")
+
+				replace("(<.-|punctuation.bracket|.-={>) *(<.-|punctuation.bracket|.-=}>)", "%1%2")
+				replace("(<.-|punctuation.bracket|.-={>) *(<.*>) *(<.-|punctuation.bracket|.-=}>)","%1 %2 %3")
+
+				replace("(<|punctuation.bracket|=%(>) *(.-) *(<|punctuation.bracket|=%)>)","%1%2%3")
+			end)
+		end
+
 		vim.api.nvim_create_autocmd("InsertLeave", {
-			callback = function()
-				vim.schedule(function ()
-					-- simpleFormat.reveal()
-					replace("(%S) -(<.-|operator|.-=.->)", "%1 %2")
-					replace("(<.-|operator|.-=.->) -(%S)", "%1 %2")
-					replace("(<.-|punctuation.delimiter|.-=,>) -(%S)", "%1 %2")
-					replace("(<.-|punctuation.bracket|.-={>) -(<.-|punctuation.bracket|.-=}>)", "%1%2")
-					replace("(<.-|punctuation.bracket|.-={>) -(%S.-%S) -(<.-|punctuation.bracket|.-=}>)", "%1 %2 %3")
-					replace("(<.-|punctuation.bracket|.-=%(>) -(%S.-%S) -(<.-|punctuation.bracket|.-=%)>)", "%1%2%3")
-					replace("(<.-|punctuation.bracket|.-=%(>) -(<.-|punctuation.bracket|.-=%)>)", "%1%2")
-				end)
-			end,
+			callback = format,
 		})
+
+		vim.api.nvim_create_user_command("Reveal", simpleFormat.reveal, {})
 	end
 }
