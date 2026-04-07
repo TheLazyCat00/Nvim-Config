d492b3f3189448ae00cf2ccc30c60659c8d1dc7f
TheLazyCat00 <manuel.stieger@proton.me>
2026-04-07 16:53:26 +0200

    update use right keymaps

--- lua/config/keymaps/visual-mode.lua
+++ lua/config/keymaps/visual-mode.lua
@@ -1,5 +1,5 @@
 return {
-	{ ".", "<cmd>norm .<CR>", desc = "Repeat last change" },
-	{ "@", "<cmd>norm @q<CR>", desc = "Execute macro 'q'" },
-	{ "Q", "<cmd>norm Q<CR>", desc = "Execute last macro" },
+	{ ".", ":normal! .<CR>", desc = "Repeat last change" },
+	{ "@", ":normal! @q<CR>", desc = "Execute macro 'q'" },
+	{ "Q", ":normal! Q<CR>", desc = "Execute last macro" },
 }
