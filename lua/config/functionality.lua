04064681ca634f2267d8f8b989046ccf4ad78685
TheLazyCat00 <manuel.stieger@proton.me>
2026-04-07 16:39:14 +0200

    add nvim files

--- lua/config/functionality.lua
+++ lua/config/functionality.lua
@@ -1,1 +1,23 @@
 -- INFO: place to put more complex logic
+
+local nvimFiles = {
+	["~/.rubocop.yml"] = {
+		"Layout/IndentationStyle:",
+		"  Enabled: false",
+		"Layout/IndentationWidth:",
+		"  Enabled: false",
+		"Style:",
+		"  Enabled: false",
+	}
+}
+
+for path, content in pairs(nvimFiles) do
+	local expanded = vim.fn.expand(path)
+	if vim.fn.filereadable(expanded) == 0 then
+		local f = io.open(expanded, "w")
+		if f then
+			f:write(type(content) == "table" and table.concat(content, "\n") or content)
+			f:close()
+		end
+	end
+end
