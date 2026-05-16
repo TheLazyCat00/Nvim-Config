602b84c5ab2804374ee175267ae4c2112fcc22f4
TheLazyCat00 <manuel.stieger@proton.me>
2026-05-17 01:55:11 +0200

    update

--- lua/plugins/namu.lua
+++ lua/plugins/namu.lua
@@ -1,50 +1,11 @@
 return {
 	"TheLazyCat00/namu.nvim",
-	lazy = false,
 	opts = {
 		namu_symbols = { -- Specific Module options
 			display = { mode = "icon", format = "tree_guides" },
-			options = {
-				AllowKinds = {
-					default = {
-						"Function",
-						-- "Method",
-						-- "Class",
-						-- "Module",
-						-- "Property",
-						-- "Variable",
-						-- "Constant",
-						-- "Enum",
-						-- "Interface",
-						-- "Field",
-						-- "Struct",
-					},
-				}
-			},
-			auto_start = {
-				enabled = true,
-				mode = "treesitter"
-			}
 		},
 		watchtower = {
 			prefer_treesitter = true,
-			options = {
-				AllowKinds = {
-					default = {
-						"Function",
-						-- "Method",
-						-- "Class",
-						-- "Module",
-						-- "Property",
-						-- "Variable",
-						-- "Constant",
-						-- "Enum",
-						-- "Interface",
-						-- "Field",
-						-- "Struct",
-					},
-				}
-			}
 		}
 	},
 	cmd = "Namu",
