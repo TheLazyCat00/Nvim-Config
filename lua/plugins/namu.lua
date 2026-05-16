1566d01b900195449667d9138143738ba0dbf61b
TheLazyCat00 <manuel.stieger@proton.me>
2026-05-17 01:54:33 +0200

    update

--- lua/plugins/namu.lua
+++ lua/plugins/namu.lua
@@ -1,13 +1,55 @@
 return {
-	"bassamsdata/namu.nvim",
+	"TheLazyCat00/namu.nvim",
+	lazy = false,
 	opts = {
 		namu_symbols = { -- Specific Module options
 			display = { mode = "icon", format = "tree_guides" },
+			options = {
+				AllowKinds = {
+					default = {
+						"Function",
+						-- "Method",
+						-- "Class",
+						-- "Module",
+						-- "Property",
+						-- "Variable",
+						-- "Constant",
+						-- "Enum",
+						-- "Interface",
+						-- "Field",
+						-- "Struct",
+					},
+				}
+			},
+			auto_start = {
+				enabled = true,
+				mode = "treesitter"
+			}
 		},
+		watchtower = {
+			prefer_treesitter = true,
+			options = {
+				AllowKinds = {
+					default = {
+						"Function",
+						-- "Method",
+						-- "Class",
+						-- "Module",
+						-- "Property",
+						-- "Variable",
+						-- "Constant",
+						-- "Enum",
+						-- "Interface",
+						-- "Field",
+						-- "Struct",
+					},
+				}
+			}
+		}
 	},
 	cmd = "Namu",
 	keys = {
-		{ "<leader>ss", "<cmd>Namu symbols<cr>", desc = "Jump to LSP symbol" },
-		{ "<leader>sw", "<cmd>Namu workspace<cr>", desc = "LSP Symbols - Workspace" }
+		{ "<leader>ss", "<cmd>Namu treesitter<cr>", desc = "Namu treesitter" },
+		{ "<leader>sw", "<cmd>Namu watchtower<cr>", desc = "Namu watchtower" }
 	}
 }
