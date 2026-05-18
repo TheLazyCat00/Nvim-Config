fd985ec7db95decce674962dc2a68cde34099ada
TheLazyCat00 <manuel.stieger@proton.me>
2026-05-18 09:56:09 +0200

    only open in first tab

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -5,24 +5,22 @@ return {
 	},
 	lazy = false,
 	opts = function()
+		-- Capture the ID of the first tab page when Neovim starts up
+		local first_tab = vim.api.nvim_get_current_tabpage()
+
 		return {
 			icons = LazyVim.config.icons.kinds,
 			layout = {
 				width = 0.2,
-
 				-- key-value pairs of window-local options for aerial window (e.g. winhl)
 				win_opts = {},
-
 				default_direction = "left",
-
 				-- Determines where the aerial window will be opened
 				--   edge   - open aerial at the far right/left of the editor
 				--   window - open aerial to the right/left of the current window
 				placement = "edge",
-
 				-- When the symbols change, resize the aerial window (within min/max constraints) to fit
 				resize_to_content = false,
-
 				-- Preserve window size equality with (:help CTRL-W_=)
 				preserve_equality = false,
 			},
@@ -31,7 +29,11 @@ return {
 			},
 			backends = { "treesitter", "markdown", "man" },
 			attach_mode = "global",
-			open_automatic = true,
+
+			-- Only open automatically if the current tab is the original first tab
+			open_automatic = function(bufnr)
+				return vim.api.nvim_get_current_tabpage() == first_tab
+			end,
 		}
 	end,
 	keys = {
