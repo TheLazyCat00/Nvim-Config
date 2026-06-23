6f0a5018ad64db2773b3caf2d524b2339abae9a0
TheLazyCat00 <manuel.stieger@proton.me>
2026-06-23 14:41:42 +0200

    update

--- lua/plugins/disableDoubles.lua
+++ lua/plugins/disableDoubles.lua
@@ -2,6 +2,6 @@ return {
 	"TheLazyCat00/disable-double-motion",
 	lazy = false,
 	config = function()
-		require("disable-double-motion").disableDoubles("o")
-	end	
+		require("disable-double-motion").disableDoubles("u")
+	end
 }
